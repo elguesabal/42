cc98ef1c081a74b7195bc4c8b27aca50a853aa11
joseanto <joseelguesabal@gmail.com>
2023-10-10 18:34:06 -0300

    fiz algumas questoes

---
+++ Turbo 4.2/c 08/ex01/ft_boolean.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joseanto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 15:42:05 by joseanto          #+#    #+#             */
+/*   Updated: 2023/10/10 16:30:24 by joseanto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef int	t_bool;
+# define TRUE 1
+# define FALSE 0
+# define EVEN(nbr) nbr % 2 == 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+#endif // FT_BOOLEAN_H
---
+++ Turbo 4.2/c 08/ex02/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "ft_abs.h"
+
+int	main(void)
+{
+	printf("%d", ABS(-50));
+	return (0);
+}
