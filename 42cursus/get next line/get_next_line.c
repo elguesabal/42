82022d52f387fcf975e3a63bb980e82780505a72
elguesabal <joseelguesabal@gmail.com>
2023-11-21 22:09:31 -0300

    atualizando

--- 42cursus/get next line/get_next_line.c
+++ 42cursus/get next line/get_next_line.c
@@ -12,6 +12,84 @@
 
 #include "get_next_line.h"
 
+char	*get_line(char *rest_line)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!rest_line[i])
+		return (NULL);
+	while (rest_line[i] && rest_line[i] != '\n')
+		i++;
+	str = (char *)malloc(sizeof(char) * (i + 2));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (rest_line[i] && rest_line[i] != '\n')
+	{
+		str[i] = rest_line[i];
+		i++;
+	}
+	if (rest_line[i] == '\n')
+	{
+		str[i] = rest_line[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*new_line(char *rest_line)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	while (rest_line[i] && rest_line[i] != '\n')
+		i++;
+	if (!rest_line[i])
+	{
+		free(rest_line);
+		return (NULL);
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(rest_line) - i + 1));
+	if (!str)
+		return (NULL);
+	i++;
+	j = 0;
+	while (rest_line[i])
+		str[j++] = rest_line[i++];
+	str[j] = '\0';
+	free(rest_line);
+	return (str);
+}
+
+char	*read_rest_line(int fd, char *rest_line)
+{
+	char	*buffer;
+	int		rd_bytes;
+
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(rest_line, '\n') && rd_bytes != 0)
+	{
+		rd_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (rd_bytes == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[rd_bytes] = '\0';
+		rest_line = ft_strjoin(rest_line, buffer);
+	}
+	free(buffer);
+	return (rest_line);
+}
+
 char	*get_next_line(int fd)
 {
 	char		*line;
