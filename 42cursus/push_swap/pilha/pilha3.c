9a8c412d683daa868210ca3bcb9676328bd8a536
joseanto <joseelguesabal@gmail.com>
2023-12-12 19:15:25 -0300

    atualizando

---
+++ 42cursus/push_swap/pilha/pilha3.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct no {
+	int	n;
+	struct no	*prox;
+} numero;
+
+int	main(void)
+{
+	numero	*topo = NULL;
+
+
+	numero	*novo = malloc(sizeof(numero));
+
+}
