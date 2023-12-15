a0020fdef15241caca8d3e6dbc8b53b173e02292
joseanto <joseelguesabal@gmail.com>
2023-12-15 19:26:06 -0300

    atualizando

---
+++ 42cursus/minitalk/signal/teste4.c
@@ -0,0 +1,21 @@
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void    func()
+{
+    printf("TESTE\n");
+}
+
+int main(void)
+{
+    signal(SIGUSR1, func);
+
+    printf("PID: %d\n", getpid());
+
+    while(1)
+    {
+        sleep(1);
+    }
+}
