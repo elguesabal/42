6f4c7a98b939402c677c3b919c44b7d1122706ac
elguesabal <joseelguesabal@gmail.com>
2024-01-01 17:15:58 -0300

    primeiro commit do ano

---
+++ 42cursus/minitalk/tentativa com signal/minitalk.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joseanto <joseanto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 15:42:20 by joseanto          #+#    #+#             */
+/*   Updated: 2023/12/18 10:34:46 by joseanto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+char	*str_client;
+char	*str_server;
+
+#endif
