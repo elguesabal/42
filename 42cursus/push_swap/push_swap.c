c06c642e5c10bd01aea86f5841a53d74c0e61c65
joseanto <joseelguesabal@gmail.com>
2024-01-23 18:28:05 -0300

    atualizando

--- 42cursus/push_swap/push_swap.c
+++ 42cursus/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: joseanto <joseanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 11:21:35 by joseanto          #+#    #+#             */
-/*   Updated: 2024/01/12 10:46:38 by joseanto         ###   ########.fr       */
+/*   Updated: 2024/01/23 18:27:17 by joseanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,68 @@
 
 #include <stdio.h>
 
-int	putnbr(int n)
+void	insert_first(Stack **no, int n)
 {
-	int		i;
-	char	c;
+	Stack	*new;
 
-	i = 0;
-	if (n == -2147483648)
+	new = malloc(sizeof(Stack));
+	if(!new)
+		return ;
+	new->n = n;
+	new->next = *no;
+	new->previous = NULL;
+	if(*no)
+		(*no)->previous = new;
+	*no = new;
+}
+
+void	insert_second(Stack **no, int n)
+{
+	
+}
+
+void	insert_penultimate(Stack **no, int n)
+{
+	
+}
+
+void	insert_last(Stack **no, int n)
+{
+	Stack	*new;
+
+	if(!new)
+		return ;
+	new->n = n;
+	if(!*no)
 	{
-		i += write(1, "-", 1);
-		i += write(1, "2", 1);
-		i += putnbr(147483648);
-		return (i);
-	}
-	if (n < 0)
-	{
-		i += write(1, "-", 1);
-		n = -n;
-	}
-	if (n >= 10)
-	{
-		i += putnbr(n / 10);
-		i += putnbr(n % 10);
+		new->next = NULL;
+		new->previous = NULL;
+		*no = new;
 	}
 	else
 	{
-		c = n + 48;
-		i += write(1, &c, 1);
+		// SL OQ FZ AKI
 	}
-	return (i);
+}
+
+void	remove_first(Stack **no, int n)
+{
+
+}
+
+void	remove_second(Stack **no, int n)
+{
+
+}
+
+void	remove_penultimate(Stack **no, int n)
+{
+	
+}
+
+void	remove_last(Stack **no, int n)
+{
+
 }
 
 // void	sa()
@@ -100,38 +133,14 @@ int	putnbr(int n)
 	
 // }
 
-void	boot(int argc, char **argv)
-{
-	printf("%d\n%s\n%s\n%s", argc, argv[0], argv[1], argv[2]);
-}
-
-// void	print_result(int size)
-// {
-// 	// int	mat1[5] = {1, 2, 3, 4, 5};
-// 	// int	mat2[5] = {'\0', '\0', '\0', '\0', '\0'};
-// 	Stack	a;
-// 	Stack	b;
-// 	int	i;
-
-// 	a.n = 10;
-// 	b.n = 20;
-// 	write(1, "---------------------------------------------------------", 58);
-// 	i = 0;
-// 	while (i < size)
-// 	{
-// 		putnbr(a.n);
-// 		write(1, " ", 1),
-// 		putnbr(b.n);
-// 		write(1, "\n", 1);
-// 		i++;
-// 	}
-// 	write(1, "- -\na b\n", 9);
-// }
-
 int	main(int argc, char **argv)
 {
-	// boot(argc, argv);
-	// print_result(argc - 1);
+	Stack	*a, *b;
 
+	a = NULL;
+	b = NULL;
+	
+	first(&a, 10);
+printf("%d", a->n);
 	return (0);
 }
--- 42cursus/push_swap/push_swap.h
+++ 42cursus/push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: joseanto <joseanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 10:49:04 by joseanto          #+#    #+#             */
-/*   Updated: 2024/01/12 11:14:04 by joseanto         ###   ########.fr       */
+/*   Updated: 2024/01/23 13:54:27 by joseanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 # define PUSH_SWAP_H
 
 # include <unistd.h>
+# include <stdlib.h>
 
-typedef struct
+typedef struct stack
 {
 	int				n;
-	struct Stack	*next;
+	struct stack	*next;
+	struct stack	*previous;
 } Stack;
+
+int	ft_atoi(const char *str);
 
 #endif
