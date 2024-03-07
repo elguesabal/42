c2bd7618e69c1a4f179831efc29a9394ae082ff0
elguesabal <joseelguesabal@gmail.com>
2024-03-07 11:20:51 -0300

    atualizando

--- 42cursus/Philosophers/teste.c
+++ 42cursus/Philosophers/teste.c
@@ -4,6 +4,7 @@
 // #include <process.h>
 #include <unistd.h>
 
+
 // void	*funcao(void *param)
 // {
 // 	printf("aaa\n");
@@ -28,27 +29,26 @@
 
 void	*funcao(void *param)
 {
-	// sleep(1);
-	// int *teste = (int *)param;
+	int *teste = (int *)param;
+	sleep(1);
 	// printf("teste: %d\n", *teste);
 	// printf("teste:\n");
-
-	while (1)
-	{
-
-	}
+	(*teste)++;
+	pthread_exit(0);
 }
 
 int	main()
 {
 	pthread_t id1, id2;
-	int	n = 10;
+	int	n1 = 1;
+	int	n2 = 2;
 
-	pthread_create(&id1, NULL, funcao, &n);
-	pthread_create(&id2, NULL, funcao, &n);
+	pthread_create(&id1, NULL, funcao, &n1);
+	pthread_create(&id2, NULL, funcao, &n1);
 
-	// printf("teste: %d\n", (int)pthread_join(id1, NULL));
-	// printf("teste: %d\n", (int)pthread_join(id2, NULL));
+	// printf("return: %d\n", (int)pthread_join(id1, NULL));
+	// printf("return: %d\n", (int)pthread_join(id2, NULL));
 	(int)pthread_join(id1, NULL);
 	(int)pthread_join(id2, NULL);
+	printf("n1: %d", n1);
 }
