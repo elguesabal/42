01c363aa3dd1a0deecd43a3cd7f44f4c1dcf6e82
elguesabal <joseelguesabal@gmail.com>
2024-03-07 18:45:52 -0300

    atualizando

--- 42cursus/Philosophers/teste.c
+++ 42cursus/Philosophers/teste.c
@@ -5,50 +5,139 @@
 #include <unistd.h>
 
 
+// //	pthread_create() pthread_join() pthread_exit()
+
 // void	*funcao(void *param)
 // {
 // 	printf("aaa\n");
 // 	sleep(3);
-// 	pthread_exit(0);
+// 	pthread_exit(0); // ESSA FUNCAO E PROIBIDA
 // }
 
 // int	main()
 // {
-// 	pthread_t	id1, id2;
+// 	pthread_t	id;
 // 	int			rc;
 
-// 	pthread_create(&id1, NULL, funcao, NULL);
-// 	// rc = (int)pthread_join(id, NULL);
-// 	printf("testeeeeee\n");
-// 	printf("teste: %d\n", (int)pthread_join(id1, NULL));
+// 	pthread_create(&id, NULL, funcao, NULL);
+// 	rc = (int)pthread_join(id, NULL);
+// 	printf("teste: %d\n", rc);
 // 	return (0);
 // }
 
 
 
 
+// //	pthread_detach()
+
+// void	*funcao(void *param)
+// {
+// 	printf("Nova thread iniciada\n");
+// 	sleep(3);
+// 	printf("Nova thread concluida\n");
+// 	return NULL;
+// }
+
+// int	main()
+// {
+// 	pthread_t	id;
+// 	int	n = 90;
+
+// 	pthread_create(&id, NULL, funcao, &n);	
+
+// 	pthread_detach(id);
+
+// 	printf("Thread principal continuando...\n");
+// 	sleep(5);
+// 	printf("Thread principal concluida\n");
+
+// 	return (0);
+// }
+
+
+
+
+// //	pthread_mutex_init() pthread_mutex_destroy()
+
+// pthread_mutex_t	mutex;
+
+// int	main()
+// {
+// 	pthread_mutex_init(&mutex, NULL);
+
+// 	// ...
+
+// 	pthread_mutex_destroy(&mutex);
+
+// 	return (0);
+// }
+
+
+
+
+// //	pthread_self() pthread_mutex_lock() pthread_mutex_unlock()
+
+pthread_mutex_t	lock;
+
 void	*funcao(void *param)
 {
-	int *teste = (int *)param;
-	sleep(1);
-	// printf("teste: %d\n", *teste);
-	// printf("teste:\n");
-	(*teste)++;
+	int *n = (int *)param;
+	int	i = 0;
+	int	temp;
+
+	printf("thread: %d\n", pthread_self());
+
+	// while (i < 100000)
+	// {
+	// 	temp = *n;
+	// 	temp++;
+	// 	*n = temp;
+	// 	i++;
+	// }
+
+	// while (i < 100000)
+	// {
+	// 	pthread_mutex_lock(&lock);
+	// 	temp = *n;
+	// 	temp++;
+	// 	*n = temp;
+	// 	pthread_mutex_unlock(&lock);
+	// 	i++;
+	// }
+
+	// temp = *n;
+	// sleep(2);
+	// temp++;
+	// *n = temp;
+	// i++;
+	
+	pthread_mutex_lock(&lock);
+	temp = *n;
+	sleep(2);
+	temp++;
+	*n = temp;
+	pthread_mutex_unlock(&lock);
+
 	pthread_exit(0);
 }
 
 int	main()
 {
 	pthread_t id1, id2;
-	int	n1 = 1;
-	int	n2 = 2;
+	int	n = 0;
 
-	pthread_create(&id1, NULL, funcao, &n1);
-	pthread_create(&id2, NULL, funcao, &n1);
+	pthread_create(&id1, NULL, funcao, &n);
+	pthread_create(&id2, NULL, funcao, &n);
 
-	// printf("return: %d\n", (int)pthread_join(id1, NULL));
-	// printf("return: %d\n", (int)pthread_join(id2, NULL));
-	(int)pthread_join(id1, NULL);
-	(int)pthread_join(id2, NULL);
-	printf("n1: %d", n1);
+	// printf("thread 1 return: %d\n", (int)pthread_join(id1, NULL));
+	// printf("thread 2 return: %d\n", (int)pthread_join(id2, NULL));
+
+	pthread_mutex_init(&lock, NULL);
+
+	pthread_join(id1, NULL);
+	pthread_join(id2, NULL);
+
+	pthread_mutex_destroy(&lock);
+
+	printf("n: %d\n", n);
 }
