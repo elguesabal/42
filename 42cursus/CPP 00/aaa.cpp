c5f22dad8c51827c1c68eb4ba86933103299f78c
elguesabal <joseelguesabal@gmail.com>
2024-07-18 02:47:53 -0300

    atualizando

--- 42cursus/CPP 00/aaa.cpp
+++ 42cursus/CPP 00/aaa.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stack>
+#include <queue>
+#include <list>
 
 // CIN
 // GOTO
@@ -116,19 +118,150 @@
 // .size()
 // .top()
 // .pop()
+// int main(void) {
+// 	std::stack <std::string> cartas;
+
+// 	cartas.push("Rei de copas");
+// 	cartas.push("Rei de espadas");
+// 	cartas.push("Rei de ouros");
+// 	cartas.push("Rei de paus");
+
+// 	std::cout << "Tamanho da pilha: " << cartas.size() << std::endl;
+// 	std::cout << "Carta do topo: " << cartas.top() << std::endl;
+// 	cartas.pop();
+// 	std::cout << "Tamanho da pilha: " << cartas.size() << std::endl;
+// 	std::cout << "Carta do topo: " << cartas.top() << std::endl;
+
+// 	return (0);
+// }
+
+
+
+// .empty()
+// int main(void) {
+// 	std::stack <int> n;
+
+// 	n.push(10);
+// 	if (n.empty() == 0)
+// 		std::cout << "pilha cheia" << std::endl;
+// 	std::cout << n.top() << std::endl;
+// 	n.pop();
+// 	if (n.empty())
+// 		std::cout << "pilha vazia" << std::endl;
+// 	return (0);
+// }
+
+
+
+// .front()
+// .back()
+// int main(void) {
+// 	std::queue <std::string> cartas;
+
+// 	cartas.push("Rei de copas");
+// 	cartas.push("Rei de espadas");
+// 	cartas.push("Rei de ouros");
+// 	cartas.push("Rei de paus");
+
+// 	std::cout << "Tamanho da fila: " << cartas.size() << std::endl;
+// 	std::cout << "Primeira carta: " << cartas.front() << std::endl;
+// 	std::cout << "Ultima carta: " << cartas.back() << std::endl;
+
+// 	while (!cartas.empty()) {
+// 		std::cout << "Primeira carta: " << cartas.front() << std::endl;
+// 		cartas.pop();
+// 	}
+// 	return (0);
+// }
+
+
+
+// .push_front()
+// int main(void) {
+// 	std::list <int> aula;
+
+// 	for (int i = 0; i < 10; i++) {
+// 		aula.push_front(i);
+// 	}
+
+// 	std::cout << "Tamanho da lista: " << aula.size() << std::endl;
+
+// 	while (!aula.empty()) {
+// 		std::cout << aula.back() << std::endl;
+// 		aula.pop_back();
+// 	}
+
+// 	// int tam = aula.size();
+// 	// for (int i = 0; i < tam; i++) {
+// 	// 	std::cout << aula.front() << std::endl;
+// 	// 	aula.pop_front();
+// 	// }
+// 	return (0);
+// }
+
+
+
+// iterator
+// .begin()
+// advence()
+// .insert()
+// int main(void) {
+// 	std::list <int> aula;
+// 	std::list <int>::iterator it;
+
+// 	for (int i = 0; i < 10; i++) {
+// 		aula.push_front(i);
+// 	}
+
+// 	it = aula.begin();
+// 	advance(it, 5);
+// 	aula.insert(it, 0);
+
+// 	while (!aula.empty()) {
+// 		std::cout << aula.back() << std::endl;
+// 		aula.pop_back();
+// 	}
+// 	return (0);
+// }
+
+
+
+// .sort()
+// int main(void) {
+// 	std::list <int> aula;
+// 	std::list <int>::iterator it;
+
+// 	aula.push_front(2);
+// 	aula.push_front(1);
+// 	aula.push_front(5);
+// 	aula.push_front(3);
+// 	aula.push_front(4);
+// 	aula.sort();
+
+// 	while (!aula.empty()) {
+// 		std::cout << aula.front() << std::endl;
+// 		aula.pop_front();
+// 	}
+// 	return (0);
+// }
+
+
+
+// .reverse()
 int main(void) {
-	std::stack <std::string> cartas;
+	std::list <int> aula;
+	std::list <int>::iterator it;
 
-	cartas.push("Rei de copas");
-	cartas.push("Rei de espadas");
-	cartas.push("Rei de ouros");
-	cartas.push("Rei de paus");
+	aula.push_front(1);
+	aula.push_front(2);
+	aula.push_front(3);
+	aula.push_front(4);
+	aula.push_front(5);
+	aula.reverse();
 
-	std::cout << "Tamanho da pilha: " << cartas.size() << std::endl;
-	std::cout << "Carta do topo: " << cartas.top() << std::endl;
-	cartas.pop();
-	std::cout << "Tamanho da pilha: " << cartas.size() << std::endl;
-	std::cout << "Carta do topo: " << cartas.top() << std::endl;
-
+	while (!aula.empty()) {
+		std::cout << aula.front() << std::endl;
+		aula.pop_front();
+	}
 	return (0);
 }
