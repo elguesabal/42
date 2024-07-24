8505cf99134a3d0c2a3f0f7cb7a3a048f2875354
elguesabal <joseelguesabal@gmail.com>
2024-07-24 00:23:05 -0300

    atualizando

--- 42cursus/CPP 00/aaa.cpp
+++ 42cursus/CPP 00/aaa.cpp
@@ -562,7 +562,33 @@
 
 
 
-int main(void) {
+class aviao {
+	public:
+		int vel = 0;
+		int velMax;
+		std::string tipo;
+		void ini(int tp);
 
+	private:
+};
+
+void aviao::ini(int tp) {
+	if (tp == 1) {
+		this->velMax = 800;
+		this->tipo = "jato";
+	} else if (tp == 2) {
+		this->velMax = 350;
+		this->tipo = "monomotor";
+	} else if (tp == 3) {
+		this->velMax = 180;
+		this->tipo = "planador";
+	}
+}
+
+int main(void) {
+	aviao *av1 = new aviao();
+
+	av1->ini(1);
+	std::cout << av1->velMax << std::endl;
 	return (0);
 }
