5e31cb12da451f152c288940ef939ceab91a8a89
joseanto <joseelguesabal@gmail.com>
2024-08-17 17:24:13 -0300

    atualizando

--- 42cursus/CPP 02/ex00/main.cpp
+++ 42cursus/CPP 02/ex00/main.cpp
@@ -1,25 +1,39 @@
 #include "Fixed.hpp"
 
-int main(void) {
-	Fixed number;
+int main( void ) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
 
+	c = b;
 
-	std::cout << number.teste() << std::endl;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
 
-	return (0);
+	return 0;
 }
 
 
 
+// int main(void) {
+// 	Fixed n1;
+// 	Fixed n2(n1);
+// 	Fixed n3;
 
-int kaio{0}; 
+// 	n3 = n1;
 
-list de inicialização 
+// 	std::cout << n1.getRawBits() << std::endl;
+// 	std::cout << n2.getRawBits() << std::endl;
+// 	std::cout << n3.getRawBits() << std::endl;
 
-std::string const  _saveoi;
+// 	n1.setRawBits(1);
+// 	n2.setRawBits(2);
+// 	n3.setRawBits(3);
 
+// 	std::cout << n1.getRawBits() << std::endl;
+// 	std::cout << n2.getRawBits() << std::endl;
+// 	std::cout << n3.getRawBits() << std::endl;
 
-Canonical::Canonical(std::string const & oi) : _saveoi(oi) {
-	_saveoi = oi;
-
-}
+// 	return (0);
+// }
