5278761f335a98eec2d1893de29849e3d1f95b60
elguesabal <joseelguesabal@gmail.com>
2024-08-20 18:41:51 -0300

    atualizando

--- 42cursus/CPP 02/ex01/main.cpp
+++ 42cursus/CPP 02/ex01/main.cpp
@@ -16,38 +16,49 @@
 
 
 
-// int main( void ) {
-// 	Fixed a;
-// 	Fixed const b( 10 );
-// 	Fixed const c( 42.42f );
-// 	Fixed const d( b );
+int main( void ) {
+	Fixed a;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
 
-// 	a = Fixed( 1234.4321f );
+	a = Fixed( 1234.4321f );
 
-// 	std::cout << "a is " << a << std::endl;
-// 	std::cout << "b is " << b << std::endl;
-// 	std::cout << "c is " << c << std::endl;
-// 	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
 
-// 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-// 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-// 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-// 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 
-// 	return 0;
-// }
-
-
-
-int main(void) {
-	// Fixed a;
-	// a.setRawBits(10);
-	// std::cout << a.getRawBits() << std::endl;
-
-	Fixed const b(10);
-	// std::cout << b.getRawBits() << std::endl;
-	std::cout << b.toInt() << std::endl;
-	std::cout << b.toFloat() << std::endl;
-
-	return (0);
+	return 0;
 }
+
+
+
+// int main(void) {
+// 	// Fixed a;
+// 	// a.setRawBits(10);
+// 	// std::cout << a.getRawBits() << std::endl;
+
+// 	// Fixed const b(10);
+// 	// // std::cout << b.getRawBits() << std::endl;
+// 	// std::cout << b.toInt() << std::endl;
+// 	// std::cout << b.toFloat() << std::endl;
+
+// 	// std::cout << std::endl;
+
+// 	// Fixed const c(5.50f);
+// 	// std::cout << c.toInt() << std::endl;
+// 	// std::cout << c.toFloat() << std::endl;
+
+// 	// std::cout << std::endl;
+
+// 	// Fixed const d(10.10f);
+// 	// std::cout << d << std::endl;
+
+// 	return (0);
+// }
--- 42cursus/CPP 02/ex02/Fixed.cpp
+++ 42cursus/CPP 02/ex02/Fixed.cpp
@@ -1,44 +1,37 @@
 #include "Fixed.hpp"
 
 Fixed::Fixed(void) : _n1(0) {
-	std::cout << "Default constructor called" << std::endl;
 	// this->_n1 = 0; // OU : _n1(0) TANTO FAZ (EU ACHO)
 }
 
 Fixed::Fixed(const Fixed &fixed) {
-	std::cout << "Copy constructor called" << std::endl;
 	*this = fixed;
 }
 
 // Fixed::Fixed(const int fixed) : _n1(fixed << _n2) {
 Fixed::Fixed(const int fixed) {
-	std::cout << "Int constructor called" << std::endl;
 	this->_n1 = fixed << _n2;
 }
 
 // Fixed::Fixed(const float fixed) : _n1((int)roundf(fixed * (1 << _n2))) {
 Fixed::Fixed(const float fixed) {
-	std::cout << "Float constructor called" << std::endl;
 	this->_n1 = (int)(roundf(fixed * (1 << _n2)));
 }
 
 Fixed &Fixed::operator = (const Fixed &fixed) {
-	std::cout << "Copy assignment operator called" << std::endl;
 	this->_n1 = fixed.getRawBits();
 	return (*this);
 }
 
 Fixed::~Fixed(void) {
-	std::cout << "Default called" << std::endl;
+	// std::cout << "Default called" << std::endl;
 }
 
 int Fixed::getRawBits(void) const {
-	// std::cout << "getRawBIts member function called" << std::endl;
 	return (this->_n1);
 }
 
 void Fixed::setRawBits(const int raw) {
-	// std::cout << "setRawBIts member function called" << std::endl;
 	this->_n1 = raw;
 }
 
