2f06193f7e5e895c5f651f2a985dce1c5a205925
elguesabal <joseelguesabal@gmail.com>
2024-10-04 00:51:22 -0300

    atualizando

---
+++ 42cursus/CPP 07/ex00/main.cpp
@@ -0,0 +1,53 @@
+#include "whatever.h"
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b) {
+	T c = a;
+	a = b;
+	b = c;
+}
+
+template <typename T>
+const T min(const T &a, const T &b) {
+	return( a < b ? a : b);
+}
+
+template <typename T>
+const T max(const T &a, const T &b) {
+	return( a > b ? a : b);
+}
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return 0;
+}
+
+// int main(void) {
+// 	int *a = new int(10);
+// 	int *b = new int(20);
+
+// 	std::cout << "a: " << *a << " b: " << *b << std::endl;
+// 	swap(a, b);
+// 	std::cout << "a: " << *a << " b: " << *b << std::endl;
+
+// 	delete a;
+// 	delete b;
+
+// 	return (0);
+// }
