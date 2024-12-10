4561befa341703584ca33663ac481c609b92616c
elguesabal <joseelguesabal@gmail.com>
2024-12-09 21:41:25 -0300

    atualizando

--- 42cursus/ft_irc/ft_irc.cpp
+++ 42cursus/ft_irc/ft_irc.cpp
@@ -4,12 +4,8 @@
 
 int main(int argc, char **argv) {
 	validation(argc, argv);
-
 	Server server(std::atoi(argv[1]), argv[2]);
-
 	std::signal(SIGINT, ctrlC); // AKI AINDA NAO SEI COMO TRATAR
-
 	server.listener();
-
 	return (0);
 }
