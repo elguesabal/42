6152d01fe44a5e4d1f9e5184b21e88541e7f3c0d
elguesabal <joseelguesabal@gmail.com>
2024-12-14 18:33:11 -0300

    atualizando

--- 42cursus/ft_irc/ft_irc.cpp
+++ 42cursus/ft_irc/ft_irc.cpp
@@ -4,7 +4,6 @@
 
 int main(int argc, char **argv) {
 // std::cout.setf(std::ios::unitbuf); // COM ISSO EU CONSIGO PRINTAR ALGO SEM DEPENDER DE \n
-// std::cout << "aaaaaaaaaaaa";
 	validation(argc, argv);
 	Server server(std::atoi(argv[1]), argv[2]);
 	std::signal(SIGINT, ctrlC); // AKI AINDA NAO SEI COMO TRATAR
